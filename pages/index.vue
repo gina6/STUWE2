a520aa1d0cac763f216fd950c44609cb4641f27d
Niklas.Buchfink <niklas.buchfink@web.de>
2022-03-09 14:57:09 +0100

    add scss

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Home Page</h1>
+  <div>
+    <h1>Home Page</h1>
+    <NuxtLink to="/display/reactionGame" class="start">Start</NuxtLink> 
+  </div>
 </template>
+<style lang="scss" scoped>
+  .start {
+    color: $color-white;
+    font-size: 1rem;
+    background-color: $color-black;
+    padding: 1rem, 2rem;
+    text-decoration: none;
+    padding: 0.25rem 0.5rem;
+    border-radius: 0.25rem;
+  }
+</style>
