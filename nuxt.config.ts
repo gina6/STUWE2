2a8934fab63b5138674ecc5a6da765a32116ee6b
Niklas.Buchfink <niklas.buchfink@web.de>
2022-03-15 13:48:29 +0100

    repush current version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,23 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    privateRuntimeConfig: {
+        API_SECRET: ''
+    },
+    publicRuntimeConfig: {
+        API_BASE: '/api'
+      },
+    buildModules: [
+        '@vueuse/nuxt',
+    ],
+  vite: {
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@import "@/assets/scss/main.scss";',
+            },
+        },
+    },
+  }
 })
+
