114dc69f7fdaa503e8d23c7c479378f0f9bb8fca
Niklas.Buchfink <niklas.buchfink@web.de>
2022-03-18 02:17:18 +0100

    styling arena multiplayer

--- pages/arena/index.vue
+++ pages/arena/index.vue
@@ -1,6 +1,74 @@
+<script setup>
+  import { io } from "socket.io-client";
+  let socket = io.connect('https://thefasterone.herokuapp.com/');
+
+  socket.on('moveUp', (step) => {
+    playerPosition[0].y -= step;
+  });
+
+  socket.on('moveDown', (step) => {
+    playerPosition[0].y += step;
+  });
+
+  socket.on('moveLeft', (step) => {
+    playerPosition[0].x -= step;
+  });
+
+  socket.on('moveRight', (step) => {
+    playerPosition[0].x += step;
+  });
+
+  socket.on('addPlayer', (playerData) => {
+    playerPosition.push(playerData);
+  })
+
+  const pointPosition = reactive([
+    {id: 1, x: 100, y: 200, color: `#00c2d7`}
+  ]) 
+  const playerPosition = reactive([
+    {id: 1, x: 300, y: 300, score: 0, color: `#00C2D7`},
+    {id: 2, x: 500, y: 300, score: 0, color: `#FF2281`},
+    {id: 2, x: 700, y: 300, score: 0, color: `#FF5F1F`},
+    {id: 2, x: 900, y: 300, score: 0, color: `#65ED4B`},
+  ]) 
+  watchEffect(() => {
+    collectTest(pointPosition[0], playerPosition[0])
+  })
+  
+  function collectTest(point, player) {
+    let dx = (point.x + 50) - (player.x + 50)
+    let dy = (point.y + 50) - (player.y + 50)
+    let distance = Math.sqrt(dx * dx + dy * dy)
+    // distance < (pointradius + playerradius) => collect
+    if (distance < 100) {
+      player.score += 1
+      point.x = Math.floor(Math.random() * 500)
+      point.y = Math.floor(Math.random() * 500)
+    }
+  }
+
+  definePageMeta({
+    layout: "custom",
+  });
+
+</script>
 <template>
   <div>
-    <h1>Reaction Game</h1>
-    <Arena />
+    <Scoreboard :players="playerPosition" />
+    <Point :x="pointPosition[0].x" :y="pointPosition[0].y" :color="pointPosition[0].color"/>
+    <Player 
+      v-for="player in playerPosition"
+      :key="player.id"
+      :x="player.x"
+      :y="player.y"
+      :score="player.score"
+      :color="player.color"
+    ></Player>
   </div>
 </template>
+<style lang="scss" scoped>
+div{
+  height: 100%;
+  width: 100%;
+}
+</style>
