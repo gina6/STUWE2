b399bde5b669500e13a184ba19a227728d9129d8
gina6 <gina.fontana@stud.hslu.ch>
2022-03-22 11:39:30 +0100

    prepare testing conditions

--- pages/arena/index.vue
+++ pages/arena/index.vue
@@ -12,9 +12,9 @@
   ]) 
   const playerPosition = reactive([
     {id: 1, x: 300, y: 300, score: 0, color: `#00C2D7`},
-    {id: 2, x: 500, y: 300, score: 0, color: `#FF2281`},
-    {id: 3, x: 700, y: 300, score: 0, color: `#FF5F1F`},
-    {id: 4, x: 900, y: 300, score: 0, color: `#65ED4B`},
+    // {id: 2, x: 500, y: 300, score: 0, color: `#FF2281`},
+    // {id: 3, x: 700, y: 300, score: 0, color: `#FF5F1F`},
+    // {id: 4, x: 900, y: 300, score: 0, color: `#65ED4B`},
   ]) 
   watchEffect(() => {
     collectTest(pointPosition[0], playerPosition[0])
@@ -27,8 +27,8 @@
     // distance < (pointradius + playerradius) => collect
     if (distance < 100) {
       player.score += 1
-      point.x = Math.floor(Math.random() * 500)
-      point.y = Math.floor(Math.random() * 500)
+      point.x = Math.floor(Math.random() * 1920)
+      point.y = Math.floor(Math.random() * 1080)
     }
   }
 
