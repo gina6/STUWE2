5ced877926874f38be6b800152b683465b09aa38
gina6 <gina.fontana@stud.hslu.ch>
2022-03-23 15:53:22 +0100

    add timer functionality (unstyled)

--- components/Scoreboard.vue
+++ components/Scoreboard.vue
@@ -14,6 +14,7 @@
     >
     <div>{{index}}</div>
     </Score>
+    <Timer />
   </div>
 </template>
 
--- components/Timer.vue
+++ components/Timer.vue
@@ -1,7 +1,20 @@
 <script setup>
-    const time = reactive(30);
+const timerInMs = reactive({time: 30000, timer: null});
+
+onMounted(() => {
+  setTimeout(() => {
+    startTimer();
+  }, 3000);
+});
+function startTimer() {
+  timerInMs.timer = setInterval(() => {
+    timerInMs.time -= 1000;
+  }, 1000);
+}
 </script>
 
 <template>
-    
+  <div>
+    {{ timerInMs.time }}
+  </div>
 </template>
