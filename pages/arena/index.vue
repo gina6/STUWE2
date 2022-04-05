d9692a45f34ce4859a4cdc9a307442be14b120a5
Niklas.Buchfink <niklas.buchfink@web.de>
2022-04-05 11:07:28 +0200

    fix point spawn

--- pages/arena/index.vue
+++ pages/arena/index.vue
@@ -19,8 +19,8 @@ function collectTest(point, player) {
   // distance < (pointradius + playerradius) => collect
   if (distance < 100) {
     player.score += 1;
-    point.x = Math.floor(Math.random() * window.width);
-    point.y = Math.floor(Math.random() * window.height);
+    point.x = 50 + Math.floor(Math.random() * (window.width-50));
+    point.y = 50 + Math.floor(Math.random() * (window.width-50));
   }
 }
 
